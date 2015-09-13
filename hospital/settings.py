ab341583e780ed3a6b15d48e41db045909b4a039
medve <antonmedve@gmail.com>
2015-09-13 23:31:50 +0300

    Set debug to False

--- hospital/settings.py
+++ hospital/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&=%wjj1hvzfag5k7sg2ow5-@lhch74(*r(xue#ay^1-@jiy3hw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
