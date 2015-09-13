98255dc0b9d0ea5752b4876739b74af6950be9da
medve <antonmedve@gmail.com>
2015-09-13 23:02:57 +0300

    Everyting works

--- static/js/form.js
+++ static/js/form.js
@@ -1,15 +1,18 @@
 function get_free_time(){
-  if( $("#id_doctor").value !== undefined  && 
-      $("#id_appointment_date").value !== undefined){
-    $.getJSON("/get_free_time/",{"doctor": $("#id_doctor").value,
-                                 "doctor": $("#id_appointment_date").value
-                               }, function(data){
+  console.log("a");
+  if( $("#id_doctor").val() && $("#id_appointment_date").val()){
+    console.log("b");
+    $.getJSON("/get_free_time/",
+              {"doctor": $("#id_doctor").val(),
+               "appointment_date": $("#id_appointment_date").val()},
+        function(data){
+          $("#id_appointment_hour").empty();
           for( var i=0; i < data['times'].length; i++ ){
             $("#id_appointment_hour").append("<option value="
                                              +data['times'][i]+">"
                                              +data['times'][i]+":00</option>");
           }
-      $("#id_appointment_hour").removeProp("disabled");
+        $("#id_appointment_hour").prop("disabled",false);
       });
   }
 }
